f68d9ecc926acf61992eed1edd6231df3d8f93d1
Joy Lin <vvlnote@hotmail.com>
2019-06-19 23:58:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function produceDrivingRange(range) {
+  return function(startingBlock, endingBlock) {
+    let num1 = parseInt(startingBlock);
+    let num2 = parseInt(endingBlock);
+    let blocks = num1 - num2;
+    blocks = (blocks > 0 ? blocks : -(blocks));
+    let differences = blocks - range;
+    if differences > 0 {
+      return (`${differences} blocks out of range`);
+    } else {
+      return (`within range by ${-differences}`);
+    };
+  };
+}
+
+function produceTipCalculator(tipRate) {
+  return function(fare) {
+    return fare * tipRate;
+  };
+}
+
+function createDriver() {
+  let driverId = 0;
+  return class {
+    constructor(name) {
+      this.id = driverId ++;
+      this.name = name;
+    };
+  };
+}
