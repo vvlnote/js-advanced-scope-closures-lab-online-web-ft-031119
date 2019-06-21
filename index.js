9aa2544d5aba496d2d846711ec35130aecbc07d7
Joy Lin <vvlnote@hotmail.com>
2019-06-21 22:28:57 +0000

    Done.

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ function produceDrivingRange(range) {
   return function(startingBlock, endingBlock) {
      let num1 = parseInt(startingBlock);
      let num2 = parseInt(endingBlock);
-     let blocks = num1 - num2;
-     console.log(`blocks = ${blocks}`);
-     blocks = (blocks > 0 ? blocks : -(blocks));
-     console.log(`range = ${range}, blocks = ${blocks}`);
+     let blocks = Math.abs(num1 - num2);
      let differences = blocks - range;
-     console.log(`differences = ${differences}`);
      if (differences > 0)  {
        return (`${differences} blocks out of range`);
      } else {
